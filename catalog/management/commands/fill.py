bff2718efaece5f698d2081ea217507b3af99a98
akex <alex1029@yandex.ru>
2023-11-14 15:01:01 +0300

    homework 20.1

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -7,8 +7,17 @@ class Command(BaseCommand):
     help = 'Clear data in the database'
     def handle(self, *args, **options):
         Category.objects.all().delete()
-        category1 = Category(name='Category 1')
-        category1.save()
+        category_list = [
+            {'name': 'название1', 'description': 'описание1'},
+            {'name': 'название2', 'description': 'описание2'}
+
+        ]
+        category_for_create = []
+        for category_item in category_list:
+            category_for_create.append(
+                Category(**category_item)
+            )
+        Category.objects.bulk_create(category_for_create)
         self.stdout.write(self.style.SUCCESS('Data clear successfully.'))
 
 
