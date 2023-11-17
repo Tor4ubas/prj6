a56fc93750921d6b8659706b3cd0df0a29cd4161
akex <alex1029@yandex.ru>
2023-11-17 15:21:33 +0300

    homework 19_2 17.11

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,33 +1,35 @@
-import json
-
 from django.core.management import BaseCommand
-
-from catalog.models import Product, Category
-from config.settings import BASE_DIR
+from catalog.models import Category, Product
+from datetime import date
 
 
 class Command(BaseCommand):
-    filename = f'{BASE_DIR}\data.json'
-
-    @staticmethod
-    def read_json():
-        with open(Command.filename, 'r', encoding='utf-8') as file:
-            product_list = json.load(file)
-        return product_list
-
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
         Product.objects.all().delete()
-        product_for_create = []
-        for product in Command.read_json():
-             #Product.objects.create(name=product['fields']['name'],
-                                    #description=product['fields']['description'],
-                                    #category=Category.objects.get(pk=product['fields']['category']),
-                                    #price=product['fields']['price'])
 
-             product_for_create.append(
-            Product(name=product['fields']['name'],
-                        description=product['fields']['description'],
-                        category_id=str(product['fields']['category']),
-                        price=product['fields']['price'])
-            )
-        Product.objects.bulk_create(product_for_create)
+        category_list = [
+            {'name': 'Категория 1', 'description': 'описание1'},
+            {'name': 'Категория 2', 'description': 'описание2'}
+        ]
+
+
+        for category_item in category_list:
+            category = Category.objects.create(**category_item)
+            self.stdout.write(self.style.SUCCESS(f'Created category: {category.name}'))
+
+        products_list = [
+            {'name': 'Продукт 1', 'description': 'Описание продукта 1', 'category': 'Категория 1', 'price': 100,
+             'date_created': date.today()},
+            {'name': 'Продукт 2', 'description': 'Описание продукта 2', 'category': 'Категория 2', 'price': 200,
+             'date_created': date.today()},
+
+        ]
+
+        for product_item in products_list:
+            category = Category.objects.get(name=product_item['category'])
+            product_item['category'] = category
+            product = Product.objects.create(**product_item)
+            self.stdout.write(self.style.SUCCESS(f'Created product: {product.name}'))
+
+        self.stdout.write(self.style.SUCCESS('Database has been filled with categories and products.'))
