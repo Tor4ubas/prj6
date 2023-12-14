b52712b416c671f16005e7dd68d980ab55bce183
akex <alex1029@yandex.ru>
2023-12-14 21:49:38 +0300

    homework 22.2 14.12

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 
 from user_app.models import User
 
 admin.site.register(User)
+
+# Отмена регистрации модели User
+admin.site.unregister(User)
+
+
+# Зарегистрировать модель User с новым классом администратора
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'is_active')
+    list_filter = ('is_active',)
+    actions = ['deactivate_users', 'activate_users']
+
+    def deactivate_users(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate_users.short_description = "Деактивировать выбранных пользователей"
+
+    def activate_users(self, request, queryset):
+        queryset.update(is_active=True)
+    activate_users.short_description = "Активировать выбранных пользователей"
--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,8 @@
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from catalog.models import NULLABLE
 
 
 class User(AbstractUser):
@@ -7,18 +10,13 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True, verbose_name='Email-адрес')
 
-    #phone = models.CharField(max_length=35, verbose_name='Номер телефона', null=True)
+    # phone = models.CharField(max_length=35, verbose_name='Номер телефона', null=True)
     phone = models.CharField(max_length=20, blank=True)
     country = models.CharField(max_length=100, verbose_name='Страна')
-    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар')
-
-    # Верификация почты пользователя
-    code_verification = models.CharField(max_length=20, verbose_name='Код верификации')
-    is_verified = models.BooleanField(default=False, verbose_name='Проверка')
+    avatar = models.ImageField(upload_to='user_app/', verbose_name='Аватар')
+    is_active = models.BooleanField(default=False, verbose_name='Активность')
+    email_verification = models.BooleanField(default=False, verbose_name='Верификация почты')
+    code_verification = models.CharField(max_length=20, verbose_name='Код верификации', **NULLABLE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-    class Meta:
-        db_table = 'myuser'
-
